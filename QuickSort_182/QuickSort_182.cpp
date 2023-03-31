a6202fe9e5bead907b1e876f156937caefbada3a
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-03-31 11:26:17 +0700

    menampilkan data array

--- QuickSort_182/QuickSort_182.cpp
+++ QuickSort_182/QuickSort_182.cpp
@@ -93,3 +93,17 @@ void q_short(int low, int high)
     q_short(j + 1, high);                                                   // Langkah algoritma No. 13 
 
 }
+
+void display() {
+    cout << "\n----------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "\n----------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
